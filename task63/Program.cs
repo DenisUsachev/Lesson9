0eafe311ba1d999372d41c85ff472c5f2d08e7fc
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-30 15:37:18 +0500

    Add Last Lesson!!!

---
+++ task63/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 63: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"*/
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int num = 1;
+
+string NumbersRec(int num, int N)
+{
+    if (num <= N) return $"{num} "+ NumbersRec(num + 1, N);
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(num, N));
---
+++ task67/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9*/
+
+int NumSumm(int value)
+{
+if (value < 10)
+    return value;
+else
+    int digit = value % 10;
+    int nextValue = value / 10;
+
+return digit + NumSumm(nextValue);
+}
+
+int num = 453;
+int result = NumSumm(num);
+System.Console.WriteLine(result);
